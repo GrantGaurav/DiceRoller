5ecd3bb11d97a923208d52ac6af8f589d4592e39
Gaurav-Semwal <grant.gaurav@ithands.biz>
2018-03-07 10:39:28 +0530

    Complete Project

--- DiceRoller/ViewController.swift
+++ DiceRoller/ViewController.swift
@@ -10,8 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    var randomDice1 = 0
+    var randomDice2 = 1
+    let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        diceChanged()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +28,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        diceChanged()
+    }
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        diceChanged()
+    }
+    func diceChanged() {
+        randomDice1 = Int(arc4random_uniform(6))
+        randomDice2 = Int(arc4random_uniform(6))
+        diceImageView1.image = UIImage(named: diceArray[randomDice1])
+        diceImageView2.image = UIImage(named: diceArray[randomDice2])
+    }
 }
 
